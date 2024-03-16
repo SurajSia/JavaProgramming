dc693f49e9b3e1b84d208a4377cd7c828094780b
SurajSai <132142902+SurajSia@users.noreply.github.com>
2024-03-16 13:34:50 +0530

    Create SortByValueFreq.java

---
+++ SortByValueFreq.java
@@ -0,0 +1,44 @@
+/// CopyRights(cc) @ G.Sai Suraj(https://www.github.com./surajsia)
+//  source :: https://www.skillrack.com/
+//  Daily Test Date :: 16-03-24
+// WARNING :: Name of the file is different , just change it what ever you want
+import java.util.*;
+import java.util.stream.*;
+class Main {
+    
+    public static void sortInOrder(String[] strarr) {
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        for(String num : strarr) {
+            if(map.containsKey(num)) {
+                map.put(num,map.get(num)+1);
+            }
+            else {
+                map.put(num,1);
+            }
+          
+        }
+        Map<String, Integer> sortedMap = map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        Set<String> set = sortedMap.keySet();
+        for(String s : set) {
+              for(int i=0;i<sortedMap.get(s);i++) {
+                  System.out.print(s+" ");
+              }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+      String[] strarr = new String[N];
+      for(int i=0;i<N;i++) {
+          strarr[i] = sc.next();
+      }
+        sortInOrder(strarr);
+    }
+}
+// input :: 6 
+//          10 20 10 20 10 30
+// output :: 30 20 20 10 10 10
+// explnation :: 30->1 , 20->2 , 10->3 so by the ascending order of the occurances...
